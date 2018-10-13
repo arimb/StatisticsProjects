8607b22a9fdfb29f1b2b574d75187fce5019bf23
Ari Meles-Braverman <arimb1999@gmail.com>
2018-10-13 15:04:36 +0300

    zebra heatmap

--- Zebra/ZebraDensity.py
+++ Zebra/ZebraDensity.py
@@ -1,17 +1,30 @@
 import csv
 import os
+import math
 
-dirpath = "C:\Users\Ari\Documents\Robotics\StatisticsProjects\Zebra\Files"
+scale = 2
+dirpath = "Files"
+red = [[0]*27 for i in range(54)]
+
 for match in os.listdir(dirpath):
-    num = match[5:]
+    mnum = match[5:]
     for team in os.listdir(dirpath+"/"+match):
         tnum =  team[:team.index('_')]
+        print(mnum + ', ' + tnum)
 
         with open(dirpath+"/"+match+"/"+team) as csvfile:
             points = []
             reader = csv.reader(csvfile)
-            for row in reader[1:]:
-                tmp = row.split(",")
-                points.append((tmp[0], tmp[1]))
+            next(reader)
+            for row in reader:
+                points.append((float(row[0]), float(row[1])))
 
-            
+            redblue = points[0][0] < 27
+            for y in range(27):
+                for x in range(54):
+                    red[x if redblue else 53-x][y if redblue else 26-y] += sum([1 if math.sqrt((x-p[0])**2+(y-p[1])**2)<scale else 0 for p in points])/len(points)
+
+with open("red.csv", "w+") as file:
+    for y in range(27):
+        for x in range(54):
+            file.write(str(x) + "," + str(y) + "," + str(red[x][y]) + "\n")
