55a71c15f73ae92e5a5c1f4508f1a82eebc02f56
Ari Meles-Braverman <arimb1999@gmail.com>
2018-11-07 20:10:50 +0200

    add city coordinates to average times

--- TravelDistanceMap/AverageTime.py
+++ TravelDistanceMap/AverageTime.py
@@ -1,6 +1,12 @@
 import csv
 import os
 
+coords = {}
+with open("cities.csv") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        coords[row["Name"]] = (row["Lng"], row["Lat"])
+
 cities = {}
 for name in os.listdir("durations"):
     with open("durations/" + name) as file:
@@ -11,8 +17,8 @@ for name in os.listdir("durations"):
         cities[name[:name.index(".")]] = tmp
 
 with open("average_times.csv", "w+") as file:
-    file.write("City,AvgDuration\n")
+    file.write("City,AvgDuration,Lng,Lat\n")
     for city in cities.items():
         average = sum([x[0]*x[1] for x in city[1]])/sum([x[1] for x in city[1]])
-        print(city[0] + " - " + str(average))
-        file.write(city[0] + "," + str(average) + "\n")
+        print(city[0])
+        file.write(city[0] + "," + str(average) + "," + coords[city[0]][0] + "," + coords[city[0]][1] + "\n")
