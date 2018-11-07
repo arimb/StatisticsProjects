1e1ee4ef31e8620341565c441ab90cb8ec9535d1
Ari Meles-Braverman <arimb1999@gmail.com>
2018-11-07 19:58:07 +0200

    average times

--- TravelDistanceMap/AverageTime.py
+++ TravelDistanceMap/AverageTime.py
@@ -2,15 +2,15 @@ import csv
 import os
 
 cities = {}
-for name in os.listdir("cities"):
-    with open("cities/" + name) as file:
+for name in os.listdir("durations"):
+    with open("durations/" + name) as file:
         reader = csv.DictReader(file)
         tmp = []
         for row in reader:
             tmp.append((float(row["Duration"]), float(row["Density"])))
         cities[name[:name.index(".")]] = tmp
 
-with open("averages.csv", "w+") as file:
+with open("average_times.csv", "w+") as file:
     file.write("City,AvgDuration\n")
     for city in cities.items():
         average = sum([x[0]*x[1] for x in city[1]])/sum([x[1] for x in city[1]])
