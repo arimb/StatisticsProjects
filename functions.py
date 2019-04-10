0550faff2d828887eadb4ebb4a61c31fcf632c6b
Ari Meles-Braverman <arimb1999@gmail.com>
2019-04-10 23:24:08 +0300

    backup bots

--- functions.py
+++ functions.py
@@ -1,10 +1,10 @@
 import requests
 
-def getTBAdata(url):
+def get_tba_data(url):
     try:
         ans = requests.get("https://www.thebluealliance.com/api/v3/" + url,
                            "accept=application%2Fjson&X-TBA-Auth-Key=gl4GXuoqG8anLUrLo356LIeeQZk15cfSoXF72YT3mYkI38cCoAmReoCSSF4XWccQ").json()
         return ans
     except:
         print("oops " + url)
-        return getTBAdata(url)
+        return get_tba_data(url)
