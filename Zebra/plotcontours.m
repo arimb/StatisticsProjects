dc7244cf2c8dd6c3c26454803d35c71b5bace5bc
Ari MB <arimb1999@gmail.com>
2019-08-17 19:54:20 +0300

    improvement

--- Zebra/plotcontours.m
+++
@@ -1,9 +0,0 @@
-red = importdata("red.csv");
-
-[xq,yq] = meshgrid(0:.2:54, 0:.2:27);
-redq = griddata(red(:,1), red(:,2), sqrt(red(:,3)), xq, yq);
-
-[C,h] = contourf(xq, yq, redq);
-h.LevelStep = 0.02;
-h.LineStyle = "none";
-colormap(hot)
