bfe84bdb4df845c0de9345aefa6fddcc792551f3
AriMB <arimb1999@gmail.com>
2022-05-21 14:06:56 +0300

    TravelDistanceMap2 suggestions

--- TravelDistanceMap2/distances.py
+++ TravelDistanceMap2/distances.py
@@ -1,12 +1,9 @@
 import json
 import csv
-from matplotlib import projections
 import requests
 from retrying import retry
 from collections import OrderedDict
-from mpl_toolkits.basemap import Basemap
-import matplotlib.pyplot as plt
-import matplotlib.colors as colors
+
 
 @retry(wait_exponential_multiplier=1000, wait_exponential_max=10000)
 def get_durations(start_pos, end_pos):
@@ -25,7 +22,7 @@ with open('TravelDistanceMap2/convention_centers.csv', 'r') as f:
     reader = csv.DictReader(f)
     for row in reader:
         conv_center_coords[row['City']] = (row['Lat'], row['Lng'])
-        conv_center_duration[row['City']] = [0, 0]
+        conv_center_duration[row['City']] = {}
 
 with open('TravelDistanceMap2/counties.json', 'r') as f:
     counties = json.load(f)
@@ -37,29 +34,11 @@ for county_key, county in counties.items():
     durations = get_durations(county["Coord"], conv_center_coords.values())[1:]
     for i, duration in enumerate(durations):
         if duration is None: continue
-        conv_center_duration[conv_center_names[i]][0] += duration * county["Count"]
-        conv_center_duration[conv_center_names[i]][1] += county["Count"]
+        conv_center_duration[conv_center_names[i]][county_key] = duration
     a += 1
 
-conv_center_duration = {key: val[0]/val[1]/3600 for key, val in conv_center_duration.items()}
-conv_center_duration = OrderedDict(sorted(conv_center_duration.items(), key=lambda x: x[1]))
-
-with open('TravelDistanceMap2/durations.csv', 'w+') as f:
+with open('TravelDistanceMap2/durations_by_county.csv', 'w+') as f:
     writer = csv.writer(f, lineterminator='\n')
-    writer.writerow(['City', 'Duration', 'Lat', 'Lng'])
+    writer.writerow(['City', *counties.keys()])
     for key, value in conv_center_duration.items():
-        writer.writerow([key, value, conv_center_coords[key][0], conv_center_coords[key][1]])
-
-map = Basemap(projection='merc', llcrnrlat=24, urcrnrlat=53, llcrnrlon=-128, urcrnrlon=-64, resolution='i')
-map.drawcoastlines(linewidth=0.25)
-map.fillcontinents(color='#cc9966', lake_color='#99ffff')
-map.drawmapboundary(fill_color='#99ffff')
-map.drawcountries()
-map.drawstates(linewidth=0.25, zorder=1)
-x,y = map([float(conv_center_coords[key][1]) for key in conv_center_duration.keys()],
-          [float(conv_center_coords[key][0]) for key in conv_center_duration.keys()])
-norm = colors.Normalize(list(conv_center_duration.values())[0], list(conv_center_duration.values())[-1])
-map.scatter(x, y, c=list(conv_center_duration.values()), s=30, marker='s', cmap='winter_r', norm=norm, edgecolors='k', linewidth=0.5, zorder=999)
-plt.colorbar(label='hours', cmap='winter_r', norm=norm, location='bottom', shrink=0.5, pad=0.05, format='%d')
-plt.title('Driving Time to US Cities')
-plt.show()
+        writer.writerow([key, *[value[county_key] if county_key in value else 1e9 for county_key in counties.keys()]])
